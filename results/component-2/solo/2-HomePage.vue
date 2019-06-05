3cd0f7235477075dc1410f896809b5b77e318585
Sarah Williams <sarah@yellowbrickcode.co.uk>
2019-06-05 16:33:20 +0100

    Component 2 solo result for comparison

---
+++ results/component-2/solo/2-HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <section id="content">
+        <h1>Welcome to the LegoLog!</h1>
+        <p>Catalogue all of your personal Lego sets to keep track of what you have, what you've built and what's yet to be built.</p>
+      </section>
+</template>
+<style>
+#content {
+  grid-column: inner-col-start 3 / span 8;
+}
+</style>
+<script>
+export default {
+    name: 'HomePage'
+}
+</script>
